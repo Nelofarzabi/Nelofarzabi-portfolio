54edc40940996e45f355a946532fbe506e3c8e40
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-09-23 20:24:40 +0430

    Update files

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -7,5 +7,5 @@ export const headerData = {
     desciption: "Hello! I am Nelofar Zabi, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving.I am always looking for opportunities to grow and improve as a programmer. ",
     image: nilofar,
     imagebw: nelofar,
-    resumePdf: 'https://docs.google.com/document/d/15bDSMo6Aw7E5s1b1sByo-aCoG_CBYkT4yZCZwnB2QvA/edit?usp=sharing'
+    resumePdf: 'https://drive.google.com/file/d/1JdhrYk-_OKkeT95sC8XyKu7HKbu9DsJh/view?usp=sharing'
 }
